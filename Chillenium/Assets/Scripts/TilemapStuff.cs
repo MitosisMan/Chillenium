b9e47c33c614fe7a96c77a91931f85f50be9e62c
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2025-02-08 15:44:33 -0600

    A* Sucks balls

--- Chillenium/Assets/Scripts/TilemapStuff.cs
+++ Chillenium/Assets/Scripts/TilemapStuff.cs
@@ -3,8 +3,8 @@ using UnityEngine.Tilemaps;
 
 public class TileTest : MonoBehaviour {
 
-    public byte[,] grid = new byte[10, 10]; // this is the array of things we own
-    // assuming the map is a 8x8 and that the border is one tile wide
+    public byte[,] grid; // this is the array of things we own
+    // assuming the map is a 5x5 and that the border is one tile wide
     
     void Start () {
         Tilemap tilemap = GetComponent<Tilemap>();
@@ -12,17 +12,19 @@ public class TileTest : MonoBehaviour {
         BoundsInt bounds = tilemap.cellBounds;
         TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
 
-        for (int x = 0; x < bounds.size.x; x++) {
-            for (int y = 0; y < bounds.size.y; y++) {
+        grid = new byte[bounds.size.x, bounds.size.y];
+        Debug.Log(bounds.size.x + " x " + bounds.size.y);
+
+        for (int y = 0; y < bounds.size.y; y++) {
+            for (int x = 0; x < bounds.size.x; x++) {
                 TileBase tile = allTiles[x + y * bounds.size.x];
                 if (tile != null) {
-                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                    grid[x,y] = 1;
+                    grid[x, y] = 1;
 
                 } else {
-                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                    grid[x,y] = 0;
                 }
             }
-        }        
+        }
     }   
 }
