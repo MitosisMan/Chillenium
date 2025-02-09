91d6a605dfbcd071bcb79faceebd8e90ec83e862
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2025-02-08 20:26:11 -0600

    Hiding refined

--- Chillenium/Assets/Scripts/Hiding Spot.cs
+++ Chillenium/Assets/Scripts/Hiding Spot.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class HidingSpot : InteractableObj
 {
+    private Vector2 loc;
+    private SpriteRenderer mysr;
+    [SerializeField] Sprite normal;
+    [SerializeField] Sprite hiding;
+
+    void Awake(){
+        mysr = GetComponent<SpriteRenderer>();
+    }
+
     public override void OnInteract(){
         if(!plint.pm.hiding){
             plint.rb.velocity = new Vector2(0, 0);
+            loc = plint.rb.position;
             plint.rb.position = transform.position;
             plint.pm.hiding = true;
+            plint.sr.color = Color.clear;
+            mysr.sprite = hiding;
+            base.Popup.SetActive(false);
         }else{
             plint.pm.hiding=false;
+            plint.sr.color = Color.white;
+            plint.rb.position = loc;
+            mysr.sprite = normal;
+            base.Popup.SetActive(true);
         }
     }
 }
